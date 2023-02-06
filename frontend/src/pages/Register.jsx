cc2601996c67a322813e389eaa1bb2851e9480b6
kimanikevin254 <kimanikevin254@gmail.com>
2023-02-06 11:24:44 +0300

    frontend routing setup

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
